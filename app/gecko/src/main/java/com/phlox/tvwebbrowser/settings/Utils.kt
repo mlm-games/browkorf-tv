c59db23257c5971c6cab2c4fb3dd15e448574f93
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-12-19 11:20:19 +0530

    fix gecko theme issue

---
+++ app/gecko/src/main/java/com/phlox/tvwebbrowser/settings/Utils.kt
@@ -0,0 +1,11 @@
+package com.phlox.tvwebbrowser.settings
+
+import org.mozilla.geckoview.GeckoRuntimeSettings
+
+fun Theme.toGeckoPreferredColorScheme(): Int {
+    return when (this) {
+        Theme.SYSTEM -> GeckoRuntimeSettings.COLOR_SCHEME_SYSTEM
+        Theme.WHITE -> GeckoRuntimeSettings.COLOR_SCHEME_LIGHT
+        Theme.BLACK -> GeckoRuntimeSettings.COLOR_SCHEME_DARK
+    }
+}
