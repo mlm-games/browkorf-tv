a5c32b99db749d8839019fd053cda20fc08b98c6
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-04-12 21:28:39 +0530

    Revert "Adapt scroll hack from upstream (#5)"
    This reverts commit fb8acccb309e7114162f8304111d38ef4a829483.

--- app/common/src/main/java/org/mlm/browkorftv/widgets/cursor/CursorDrawerDelegate.kt
+++ app/common/src/main/java/org/mlm/browkorftv/widgets/cursor/CursorDrawerDelegate.kt
@@ -134,10 +134,6 @@ class CursorDrawerDelegate(val context: Context, val surface: View) {
         cursorSpeed.set(0f, 0f)
         surface.removeCallbacks(cursorUpdateRunnable)
         surface.removeCallbacks(longPressRunnable)
-        if (dpadCenterPressed) {
-            dispatchMotionEvent(cursorPosition.x, cursorPosition.y, MotionEvent.ACTION_CANCEL)
-            dpadCenterPressed = false
-        }
         if (scrollHackStarted) {
             dispatchMotionEvent(scrollHackCoords.x, scrollHackCoords.y, MotionEvent.ACTION_CANCEL)
             scrollHackStarted = false
@@ -341,7 +337,7 @@ class CursorDrawerDelegate(val context: Context, val surface: View) {
                     centerButtonDownTime = SystemClock.uptimeMillis()
                     surface.keyDispatcherState.startTracking(event, this)
 
-                    if (!isCursorDisappear && !directionalNavMode && !dpadCenterPressed) {
+                    if (!isCursorDisappear && !directionalNavMode) {
                         dpadCenterPressed = true
                         dispatchMotionEvent(
                             cursorPosition.x,
@@ -422,11 +418,8 @@ class CursorDrawerDelegate(val context: Context, val surface: View) {
             action, 1, properties,
             pointerCoords, 0, 0, 1f, 1f, 0, 0, 0, 0
         )
-        try {
-            surface.dispatchTouchEvent(motionEvent)
-        } finally {
-            motionEvent.recycle()
-        }
+        surface.dispatchTouchEvent(motionEvent)
+        motionEvent.recycle()
     }
 
     private fun handleDirectionKeyEvent(event: KeyEvent, x: Int, y: Int, keyDown: Boolean) {
@@ -607,10 +600,6 @@ class CursorDrawerDelegate(val context: Context, val surface: View) {
             if (cursorDirection.x == 0 && cursorDirection.y == 0 &&
                 cursorSpeed.x == 0f && cursorSpeed.y == 0f
             ) {
-                if (scrollHackStarted) {
-                    dispatchMotionEvent(scrollHackCoords.x, scrollHackCoords.y, MotionEvent.ACTION_CANCEL)
-                    scrollHackStarted = false
-                }
                 surface.postDelayed(cursorHideRunnable, CURSOR_DISAPPEAR_TIMEOUT.toLong())
                 return
             }
